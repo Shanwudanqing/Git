f9fc2859ec477c82cdabc4fa58654edb0313e4a8
Shanwudanqing <1126345108@qq.com>
2022-05-19 20:57:28 +0800

    hot-fix first commit

--- src/main/java/lesson/git/GitTest.java
+++ src/main/java/lesson/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("Hello,Git!");
         System.out.println("Hello,GitHub!");
         System.out.println("Hello,GitEE!");
+        System.out.println("Hello,Maven");
     }
 
 }
