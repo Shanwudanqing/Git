79bb5a4bea8dc5ecaf5e17ab6bb8dc0beec86723
Shanwudanqing <1126345108@qq.com>
2022-05-19 20:26:26 +0800

    third commit

--- src/main/java/lesson/git/GitTest.java
+++ src/main/java/lesson/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello,Git!");
         System.out.println("Hello,GitHub!");
+        System.out.println("Hello,GitEE!");
     }
 
 }
