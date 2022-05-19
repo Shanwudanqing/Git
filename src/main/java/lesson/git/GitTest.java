6b028a848eaba800fc1e90c739ab5420851475ac
Shanwudanqing <1126345108@qq.com>
2022-05-19 20:20:28 +0800

    first commit

---
+++ src/main/java/lesson/git/GitTest.java
@@ -0,0 +1,14 @@
+package lesson.git;
+
+/**
+ * @Description:
+ * @Author: Wanzhen
+ * @Date: 2022/5/19-20:13
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello,Git!");
+    }
+
+}
