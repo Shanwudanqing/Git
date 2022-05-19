368823f5a1e61678d200cfbdcd193d1afc2eb714
Shanwudanqing <1126345108@qq.com>
2022-05-19 22:02:25 +0800

    master forth commit

--- src/main/java/lesson/git/GitTest.java
+++ src/main/java/lesson/git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("Hello,GitHub!");
         System.out.println("Hello,GitEE!");
         System.out.println("Hello,Maven");
+        System.out.println("master test");
     }
 
 }
